a5fb9f9a99ff3a69d11dfda7f16d8ae6e05e6f4c
Anit <95236970+anitthomas@users.noreply.github.com>
2024-08-09 11:38:51 +0530

    Update lex_example1.c

--- lex_example1.c
+++ lex_example1.c
@@ -1,17 +1,22 @@
 %{ 
 #include <stdio.h> 
+int c=0;
 %}
 %% 
-[if|else]+ printf("KEYWORD: %s\n",yytext);
+
+[if|else]+ {printf("KEYWORD: %s\n",yytext);c++;}
 [0-9]+    printf("NUMBER: %s\n", yytext); 
 [a-zA-Z]+  printf("WORD: %s\n", yytext); 
-[ \t]+                               
-.     printf("UNKNOWN: %s\n", yytext);
+[ \t]+ 
+.          printf("UNKNOWN: %s\n", yytext);                     
+[\n] {return 0;}
+
 %%
 
  int main() 
   {
     yylex();
+    printf("The number of keywords is %d",c);
     return 0; 
    }
    
@@ -19,6 +24,7 @@
  {
   return 1;
   }
+
 --------------------------------------------------------
   lex lexex.l
   gcc lex.yy.c
